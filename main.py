0b506c8c26be14dfdd26b65dbc14de9c787482f6
Iver Ullrik Hepsø <iverullrik92@gmail.com>
2019-08-28 13:50:38 +0200

    Text updating now works, and error handiling added

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ class MainBody(TabbedPanel):
     def __init__(self, **kwargs):
         super(MainBody, self).__init__(**kwargs)
 
-        self.result1: str = StringProperty()
-        self.result2: str = StringProperty()
+        self.res_label1 = None
+        self.res_label2 = None
 
         self.text1 = None
         self.text2 = None
@@ -53,21 +53,21 @@ class MainBody(TabbedPanel):
         ################################################################################################################
         cuttingspeed_layout = BoxLayout(orientation="horizontal", size_hint_y=None, height="40dp")
 
-        self.text1 = MyTextInput(hint_text="m/min", multiline=False, write_tab=False, font_size=20)
+        self.text1 = MyTextInput(hint_text="m/min", multiline=False, write_tab=False, font_size=20, on_text_validate=self.calculate)
         cuttingspeed_layout.add_widget(Label(text="Cutting Speed:", font_size=20))
         cuttingspeed_layout.add_widget(self.text1)
 
         ################################################################################################################
         milldia_layout = BoxLayout(orientation="horizontal", size_hint_y=None, height="40dp")
 
-        self.text2 = MyTextInput(hint_text="ø", multiline=False, write_tab=False, font_size=20)
+        self.text2 = MyTextInput(hint_text="ø", multiline=False, write_tab=False, font_size=20, on_text_validate=self.calculate)
         milldia_layout.add_widget(Label(text="Mill Diameter:", font_size=20))
         milldia_layout.add_widget(self.text2)
 
         ################################################################################################################
         numteeth_layout = BoxLayout(orientation="horizontal", size_hint_y=None, height="40dp")
 
-        self.text3 = MyTextInput(hint_text="z", multiline=False, write_tab=False, font_size=20)
+        self.text3 = MyTextInput(hint_text="z", multiline=False, write_tab=False, font_size=20, on_text_validate=self.calculate)
         numteeth_layout.add_widget(Label(text="Number of Teeths:", font_size=20))
         numteeth_layout.add_widget(self.text3)
 
@@ -89,18 +89,18 @@ class MainBody(TabbedPanel):
         ################################################################################################################
         result_layout1 = BoxLayout(orientation="horizontal", size_hint_y=None, height="40dp")
 
-        res_label1 = Label(text=self.result1, font_size=30)
+        self.res_label1 = Label(text="", font_size=30)
 
         result_layout1.add_widget(Label(text="Spindel RPM: ", font_size=20))
-        result_layout1.add_widget(res_label1)
+        result_layout1.add_widget(self.res_label1)
 
         ################################################################################################################
         result_layout2 = BoxLayout(orientation="horizontal", size_hint_y=None, height="40dp")
 
-        res_label2 = MyLabel(text=self.result2, font_size=30, bcolor=[1, 1, 1, 0.2])
+        self.res_label2 = MyLabel(text="", font_size=30, bcolor=[1, 1, 1, 0.2])
 
         result_layout2.add_widget(MyLabel(text="Feedrate: ", font_size=20, bcolor=[1, 1, 1, 0.2]))
-        result_layout2.add_widget(res_label2)
+        result_layout2.add_widget(self.res_label2)
 
         ################################################################################################################
         main_layout.add_widget(cuttingspeed_layout)
@@ -115,24 +115,32 @@ class MainBody(TabbedPanel):
         self.tab1.add_widget(main_layout)
 
     def calculate(self, *args):
-        print(self.text1.text)
-        print(self.text2.text)
-        print(self.text3.text)
-        print(self.text4.text)
 
         try:
             cuttingspeed = float(self.text1.text.replace(',', '.'))
+        except ValueError:
+            cuttingspeed = 0
+
+        try:
             milldia = float(self.text2.text.replace(',', '.'))
+        except ValueError:
+            milldia = 0
+
+        try:
             numz = float(self.text3.text.replace(',', '.'))
+        except ValueError:
+            numz = 0
+
+        try:
             feedprtooth = float(self.text4.text.replace(',', '.'))
-        except ValueError as e:
-            print(e)
+        except ValueError:
+            feedprtooth = 0
 
         spindelspeed = spindel_rpm(cuttingspeed, milldia)
         feed = feedrate(spindelspeed, numz, feedprtooth)
 
-        self.result1 = str(round(spindelspeed, 0))
-        self.result2 = str(round(feed, 0))
+        self.res_label1.text = str(int(round(spindelspeed, 0)))
+        self.res_label2.text = str(int(round(feed, 0)))
 
 
 class CncCalculators(App):
