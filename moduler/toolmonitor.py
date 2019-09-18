d60af1374621b6c7081883034044fcda2f349dc3
Iver Ullrik Hepsø <iverullrik92@gmail.com>
2019-09-18 16:43:41 +0200

    Piece count list is up

--- moduler/toolmonitor.py
+++ moduler/toolmonitor.py
@@ -20,9 +20,21 @@ class ToolMonitor(BoxLayout):
         super(ToolMonitor, self).__init__(**kwargs)
 
         self.master = tab_controll
-        # self.grid1 = GridLayout(cols=1, spacing=1, padding=10)
-        self.grid1 = BoxLayout(orientation='vertical')
-        self.grid2 = GridLayout(cols=1, spacing=1, padding=10)
+
+        self.grid1 = BoxLayout(orientation='vertical', padding=10)
+
+        self.grid2 = BoxLayout(orientation='vertical', spacing=1)
+
+        self.grid3 = GridLayout(cols=1, spacing=1, padding=10, size_hint_y=None)
+        self.grid3.bind(minimum_height=self.grid3.setter('height'))
+
+        self.scrollwidget = None
+
+        # ---------------------------------------------------------------------
+
+        self.grey = [1, 1, 1, 0.2]
+
+        #######################################################################
 
         # Config generation
         # ---------------------------------------------------------------------
@@ -39,7 +51,10 @@ class ToolMonitor(BoxLayout):
         self.exceldatabase = Database(self.config_path)
 
         self._special_tools()
-        self.skrot()
+        self.testing()
+        self._parts_possible()
+
+        print(self.children)
 
         self.master.add_widget(self)
 
@@ -68,26 +83,49 @@ class ToolMonitor(BoxLayout):
         special_tools = self.exceldatabase.special_tools
         special_tools.sort()
 
-        self.grid1.add_widget(MyLabel(text='Special Tools:', font_size=20, bcolor=[1, 1, 1, 0.2]))
+        self.grid1.add_widget(MyLabel(text='Special Tools:', font_size=20, bcolor=self.grey))
 
         for i in special_tools:
             self.grid1.add_widget(Label(text=i, font_size=20))
 
         self.add_widget(self.grid1)
 
+    def testing(self):
+
+        btn1 = Button(text='Calculate', size_hint_y=None, height=30)
+        btn1.bind(on_press=self.test2)
+
+        btn2 = Button(text='Clear', size_hint_y=None, height=30)
+        btn2.bind(on_press=self.test3)
+
+        self.grid2.add_widget(btn1)
+        self.grid2.add_widget(btn2)
+        self.grid2.add_widget(Button(text='Grab new data', size_hint_y=None, height=30))
+        self.grid2.add_widget(Label())
+
+        self.add_widget(self.grid2)
+
+    def test2(self, touch):
+
+        self.remove_widget(self.scrollwidget)
+
+    def test3(self, touch):
+
+        self._parts_possible()
+
     def _parts_possible(self):
-        pass
 
-    def skrot(self):
+        toolinfo = GibbsCam(self.config_path)
+        tools_in_use = toolinfo.piece_count
+        max_pieces = toolinfo.max_piece_count
 
-        test2 = GridLayout(cols=1, spacing=7, padding=10, size_hint_y=None)
-        test2.bind(minimum_height=test2.setter('height'))
+        self.grid3.add_widget(MyLabel(text='Tools in use:', size_hint_y=None, height=45, font_size=20, bcolor=self.grey))
 
-        for i in range(100):
-            test2.add_widget(Label(text=f"Tool{i + 1}", size_hint_y=None, height=20))
+        for i, j in zip(tools_in_use, max_pieces):
+            self.grid3.add_widget(Label(text=f'{i} : {tools_in_use[i]}/{max_pieces[i]}', size_hint_y=None, height=30, font_size=20))
 
-        scroll_test = ScrollView(size_hint=(1, None), size=(Window.width, Window.height - 75))
-        scroll_test.add_widget(test2)
+        self.scrollwidget = ScrollView(size_hint=(1, None), size=(Window.width, Window.height - 57))
+        self.scrollwidget.add_widget(self.grid3)
 
-        self.add_widget(scroll_test)
+        self.add_widget(self.scrollwidget)
 
